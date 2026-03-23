00a124df3d24e4e2762bfcef150dc7ceaf054039
Murilo Domingues <murilofcd@gmail.com>
2026-03-22 21:57:07 -0300

    Add age calculation from days to years, months, days
    Implement age calculation in years, months, and days from total days.

---
+++ 1020_Idade_em_Dias.py
@@ -0,0 +1,25 @@
+'''
+Leia um valor inteiro correspondente à idade de uma pessoa em dias e informe-a em anos, meses e dias
+
+Obs.: apenas para facilitar o cálculo, considere todo ano com 365 dias e todo mês com 30 dias. Nos casos de teste nunca haverá uma situação que permite 12 meses e alguns dias, como 360, 363 ou 364. Este é apenas um exercício com objetivo de testar raciocínio matemático simples.
+
+Entrada
+
+O arquivo de entrada contém um valor inteiro.
+
+Saída
+
+Imprima a saída conforme exemplo fornecido.
+'''
+
+dias_totais = int(input())
+
+anos = dias_totais // 365
+resto = dias_totais % 365
+
+meses = resto // 30
+dias = resto % 30
+
+print(f"{anos} ano(s)")
+print(f"{meses} mes(es)")
+print(f"{dias} dia(s)")
